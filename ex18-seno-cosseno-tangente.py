5fa4046da6882bf7a0ce3d81f469e736540cf401
Ollywer <dyegoollywer@gmail.com>
2019-09-12 12:26:58 -0300

    Atualização

--- ex18-seno-cosseno-tangente.py
+++ ex18-seno-cosseno-tangente.py
@@ -1,3 +1,3 @@
-from math import sin, cos, tan
-angulo = float(input('Digite um Ângulo: '))
-print('Seno: {}\n Cosseno: {}\n Tangente: {}'.format(sin(angulo), cos(angulo), tan(angulo)))
+from math import cos, tan, sin
+angulo = int(input('Digite um angulo: '))
+print('Seno: {}\nCosseno: {}\nTangente: {}'.format(sin(angulo), cos(angulo), tan(angulo)))
