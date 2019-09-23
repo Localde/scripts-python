e9bdd31ec86153ba66fa76d0e5600f9c4e2ee2f2
Ollywer <dyegoollywer@gmail.com>
2019-09-23 10:59:14 -0300

    Atualização

--- ex19-sorteando-um-item-na-lista.py
+++ ex19-sorteando-um-item-na-lista.py
@@ -1,18 +1,10 @@
-#import random
-#nome1 = str(input('Digite um nome: '))
-#nome2 = str(input('Digite outro nome: '))
-#nome3 = str(input('Digite outro nome: '))
-#nome4 = str(input('Digite outro nome: '))
-#lista = [nome1, nome2, nome3, nome4]
-#sorteado = random.choice(lista)
+from random import sample
 
-#print('O escolhido foi {}.'.format(sorteado))
-from random import choice
-nome1 = str(input('Digite um nome: '))
-nome2 = str(input('Digite outro nome: '))
-nome3 = str(input('Digite outro nome: '))
-nome4 = str(input('Digite outro nome: '))
-lista = [nome1, nome2, nome3, nome4]
-sorteado = choice(lista)
+nome_1 = str(input('Digite um nome: '))
+nome_2 = str(input('Digite outro nome: '))
+nome_3 = str(input('Digite outro nome: '))
+nome_4 = str(input('Digite outro nome: '))
 
-print('O escolhido foi {}.'.format(sorteado))
+names = [nome_1, nome_2, nome_3, nome_4]
+
+print('O escolhido foi {}'.format(sample(names, 1)))
