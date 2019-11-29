20165ab492da7db99441aeb5b9ac8e55c5c7ea69
Ollywer <dyegoollywer@gmail.com>
2019-11-29 13:21:08 -0300

    Atualização

--- ex75-analise-de-dados-em-uma-tupla.py
+++ ex75-analise-de-dados-em-uma-tupla.py
@@ -1,21 +1,14 @@
 valores = (
-    int(input('Digite um numero: ')),
-    int(input('Digite outro numero: ')),
-    int(input('Digite outro numero: ')),
-    int(input('Digite outro numero: '))
+    int(input('Digite um valor: ')),
+    int(input('Digite outro valor: ')),
+    int(input('Digite outro valor: ')),
+    int(input('Digite outro valor: '))
 )
 
-if 9 in valores:
-    print(f'Numero de vezes que apareceu o 9: {valores.count(9)} vezes.')
-else:
-    print('Não apareceu o numero 9.')
-if 3 in valores:
-    print(f'O numero 3 aparece pela primeira vez na {valores.index(3)} posição.')
-else:
-    print('Não apareceu o numero 3.')
-print('Os valores pares são: ', end=' ')
-for valor in valores:
-    if valor % 2 == 0:
-        print(f'{valor} ', end='')
-    else:
-        continue
+print(f'Quantidade de numero 9 digitado: {valores.count(9)}')
+print(f'Posição do 1º numero 3 digitado: {valores.index(3) + 1}')
+print('Os numeros pares são: ', end='')
+
+for a in range(0, 4):
+    if valores[a] % 2 == 0:
+        print(valores[a], end=' ')
