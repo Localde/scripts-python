e71357cae7d6290efc1b7d45871ef0839f384687
Ollywer <dyegoollywer@gmail.com>
2019-12-01 11:18:41 -0300

    Atualização

--- ex74-maior-e-menor-valores-em-tupla.py
+++ ex74-maior-e-menor-valores-em-tupla.py
@@ -1,20 +1,8 @@
 from random import randint
-a = randint(0, 10)
-b = randint(0, 10)
-c = randint(0, 10)
-d = randint(0, 10)
-e = randint(0, 10)
-tupla = (a, b, c, d, e)
-verifica = 0
-for variavel in tupla:
-    if verifica == 0:
-        maior = variavel
-        menor = variavel
-        verifica += 1
-    elif variavel > maior:
-        maior = variavel
-    elif variavel < menor:
-        menor = variavel
-print(tupla)
-print(f'Maior = {maior}')
-print(f'Menor = {menor}')
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print('Os valores sorteados foram: ', end=' ')
+for n in numeros:
+    print(f'{n} ', end='')
+print()
+print(f'O maior valor sorteado foi {max(numeros)}')
+print(f'O menor valor sorteado foi {min(numeros)}')
