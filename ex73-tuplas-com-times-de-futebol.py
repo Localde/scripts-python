2a612ab42e5709f7e0581942a0bf44df0af2478f
Ollywer <dyegoollywer@gmail.com>
2019-12-02 11:25:34 -0300

    Atualização

--- ex73-tuplas-com-times-de-futebol.py
+++ ex73-tuplas-com-times-de-futebol.py
@@ -1,6 +1,5 @@
-os_20_primeiros_colocados_do_brasileirão_de_futebol = 'Flamengo', 'Palmeiras', 'Santos', 'Grêmio', 'São Paulo', 'Athletico', 'Internacional', 'Corinthians', 'Bahia', 'Vasco da Gama', 'Goiás', 'Atlético', 'Fortaleza', 'Botafogo', 'Ceára SC', 'Cruzeiro', 'Fluminense', 'CSA', 'Chapecoense', 'Avaí'
-
-print(os_20_primeiros_colocados_do_brasileirão_de_futebol[:5])
-print(os_20_primeiros_colocados_do_brasileirão_de_futebol[-4:])
-print(sorted(os_20_primeiros_colocados_do_brasileirão_de_futebol))
-print(os_20_primeiros_colocados_do_brasileirão_de_futebol.index('Chapecoense') + 1)
+tupla = 'ba', 'eb', 'fc', 'chapecoense', 'ed', 'ge', 'yf', 'hg', 'yh', 'si', 'cj' 'sk', 'jl', 'rm', 'an', 'eo', 'ip', 'oq', 'fu', 'fv', 'vx'
+print(tupla[:5])
+print(tupla[-4:])
+print(sorted(tupla))
+print(tupla.index('chapecoense')+1)
